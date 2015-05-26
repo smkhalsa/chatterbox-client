a43fc3abb4fd51718e441565d14bcecee1e2d4d5
smkhalsa <smkhalsa@gmail.com>
2015-05-25 20:11:43 -0700

    Half-working room filter

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,27 @@
 // YOUR CODE HERE:
 var requestTime = '2014-05-25T23:19:10.140Z';
-var data = function() {
+var roomOptions = {};
+var updateRooms = function(room){
+  if(!roomOptions[room]) {
+    roomOptions[room] = true;
+    $('#roomSelect').append('<option class="roomItem" data="' + room +'">' + room + '</option>');
+  }
+};
+var data = function(){
   $.ajax({
     url:'https://api.parse.com/1/classes/chatterbox',
     context: document.body
   }).done(function(data){
     for(var i = 0; i < data.results.length; i++) {
-      if(data.results[i].text && data.results[i].username && new Date(data.results[i].createdAt) > new Date(requestTime)) {
+      if(data.results[i].text && data.results[i].username && new Date(data.results[i].createdAt) > new Date(requestTime)
+        && data.results[i].roomname) {
+        updateRooms(data.results[i].roomname);
         var user = data.results[i].username.toString().replace(/<.*?>/, "");
         var message = data.results[i].text.toString().replace(/<.*?>/, "");
+        var room = data.results[i].roomname.toString().replace(/<.*?>/, "");
 
-        $('#messages').prepend('<div class="message">' +'User: ' + user +
-        ' Message: '+ message + '</div>');
+        $('#messages').prepend('<div class="'+room+'">' +'User: ' + user +
+        ' Message: '+ message + ' Room: ' + room + '</div>');
       }
     }
     requestTime = data.results[0].createdAt;
@@ -24,10 +34,14 @@ setInterval(data, 1000);
 $(document).ready(function(){
   $('#submitButton').click(function(){
     // debugger;
+    var roomAssignment = $('#roomSelect').val();
+    if(roomAssignment === 'New Room') {
+      roomAssignment = prompt('Create a new room');
+    }
     var message = {
       'username': name,
       'text': $('#messageInput').val(),
-      'roomname': ''
+      'roomname': roomAssignment
     };
     $.ajax({
       url: 'https://api.parse.com/1/classes/chatterbox',
@@ -41,5 +55,14 @@ $(document).ready(function(){
         console.error('chatterbox: Failed to send message');
       }
     });
-  })
+  });
+
+  $('#roomSelect').on('change', function(){
+    // $('#messages').hide();
+    // $('.' + $('#roomSelect').val()).show();
+    var selection = '.' + $('#roomSelect').val();
+    // debugger;
+    $('#messages').children().not(selection).hide();
+  });
+
 });
