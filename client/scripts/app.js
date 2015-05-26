e5f49f17376adc65e296361a59c8897ebdc3303c
smkhalsa <smkhalsa@gmail.com>
2015-05-26 11:31:48 -0700

    Finished filter and improved escaping

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,37 +1,44 @@
 // YOUR CODE HERE:
-var requestTime = '2014-05-25T23:19:10.140Z';
-var roomOptions = {};
-var updateRooms = function(room){
-  if(!roomOptions[room]) {
-    roomOptions[room] = true;
-    $('#roomSelect').append('<option class="roomItem" data="' + room +'">' + room + '</option>');
-  }
-};
-var data = function(){
-  $.ajax({
-    url:'https://api.parse.com/1/classes/chatterbox',
-    context: document.body
-  }).done(function(data){
-    for(var i = 0; i < data.results.length; i++) {
-      if(data.results[i].text && data.results[i].username && new Date(data.results[i].createdAt) > new Date(requestTime)
-        && data.results[i].roomname) {
-        updateRooms(data.results[i].roomname);
-        var user = data.results[i].username.toString().replace(/<.*?>/, "");
-        var message = data.results[i].text.toString().replace(/<.*?>/, "");
-        var room = data.results[i].roomname.toString().replace(/<.*?>/, "");
-
-        $('#messages').prepend('<div class="'+room+'">' +'User: ' + user +
-        ' Message: '+ message + ' Room: ' + room + '</div>');
-      }
+$(document).ready(function(){
+  var requestTime = '2014-05-25T23:19:10.140Z';
+  var roomOptions = {};
+  var currentRoom = 'all';
+  var friends = {};
+  var updateRooms = function(room){
+    if(!roomOptions[room]) {
+      roomOptions[room] = true;
+      $('#roomSelect').append('<option class="roomItem" data="' + room +'">' + room + '</option>');
     }
-    requestTime = data.results[0].createdAt;
-  });
-};
+  };
+  var data = function(){
+    $.ajax({
+      url:'https://api.parse.com/1/classes/chatterbox',
+      context: document.body
+    }).done(function(data){
+      for(var i = 0; i < data.results.length; i++) {
+        if(data.results[i].text && data.results[i].username && new Date(data.results[i].createdAt) > new Date(requestTime)
+          && data.results[i].roomname) {
+          var user = _.escape(data.results[i].username.toString());
+          var message = _.escape(data.results[i].text.toString());
+          var room = _.escape(data.results[i].roomname.toString()).replace(/ /, "_");
+          updateRooms(room);
+
+          if(currentRoom === room || currentRoom === 'all') {
+            $('#messages').prepend('<div class="all '+room+'">' +'<span class="' + user + '">' + user + '</span>' +
+            ' Message: '+ message + ' Room: ' + room + '</div>');
+          } else {
+            $('#messages').prepend('<div class="all '+room+'">' +'<span class="' + user + '">' + user + '</span>' +
+            ' Message: '+ message + ' Room: ' + room + '</div>').hide();
+          }
+        }
+      }
+      requestTime = data.results[0].createdAt;
+    });
+  };
 
 // data();
-setInterval(data, 1000);
+  setInterval(data, 1000);
 
-$(document).ready(function(){
   $('#submitButton').click(function(){
     // debugger;
     var roomAssignment = $('#roomSelect').val();
@@ -57,12 +64,23 @@ $(document).ready(function(){
     });
   });
 
+
   $('#roomSelect').on('change', function(){
     // $('#messages').hide();
     // $('.' + $('#roomSelect').val()).show();
-    var selection = '.' + $('#roomSelect').val();
-    // debugger;
-    $('#messages').children().not(selection).hide();
+
+
+    currentRoom = $('#roomSelect').val();
+
+    $('#messages').children().filter('.'+ currentRoom).show();
+    $('#messages').children().not('.'+ currentRoom).hide();
+  });
+
+  $('span').click(function() {
+    debugger;
+  var user = $(this).attr('class');
+  console.log(user);
+    // if(this.)
   });
 
 });
